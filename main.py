5c10393a743a03e5ef4c296f9c74ae7b07fb1a25
Richard H <blendblob@gmail.com>
2014-05-11 19:00:50 -0700

    inverse angle congruencies, offset*scale change

--- main.py
+++ main.py
@@ -125,13 +125,13 @@ def instanceExport(context):
                 TD_STRING+="\tLocation=(X="+str(ob.location.x*TD_SCALE)+",Y="+str(-1*ob.location.y*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"
             if isWall==True:
                 if (yawNum>=65530 or yawNum<=10):#0||360#forward
-                    TD_STRING+="\tLocation=(X="+str((ob.location.x-4)*TD_SCALE)+",Y="+str(-1*(ob.location.y+2)*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"        
-                if yawNum<=32790 and yawNum>=32750:#180#back   
-                    TD_STRING+="\tLocation=(X="+str((ob.location.x-0)*TD_SCALE)+",Y="+str(-1*(ob.location.y+2)*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"        
-                if yawNum<=16390 and yawNum>=16380:#90#left  16383.75
-                    TD_STRING+="\tLocation=(X="+str((ob.location.x-2)*TD_SCALE)+",Y="+str(-1*(ob.location.y-0)*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"        
-                if yawNum<=65540 and yawNum>=65530:#270#right   65535
-                    TD_STRING+="\tLocation=(X="+str((ob.location.x-2)*TD_SCALE)+",Y="+str(-1*(ob.location.y+4)*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"        
+                    TD_STRING+="\tLocation=(X="+str((ob.location.x-0.08)*TD_SCALE)+",Y="+str(-1*(ob.location.y+0.04)*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"        
+                if (yawNum<=32790 and yawNum>=32750) or (yawNum<=-32790 and yawNum>=-32750) :#180#back   
+                    TD_STRING+="\tLocation=(X="+str((ob.location.x-0)*TD_SCALE)+",Y="+str(-1*(ob.location.y+0.04)*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"        
+                if (yawNum<=16390 and yawNum>=16380) or (yawNum<=-65540 and yawNum>=-65530):#90#left  16383.75 -270
+                    TD_STRING+="\tLocation=(X="+str((ob.location.x-0.04)*TD_SCALE)+",Y="+str(-1*(ob.location.y-0)*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"        
+                if (yawNum<=65540 and yawNum>=65530) or (yawNum<=-16390 and yawNum>=-16380):#270||-90#right   65535
+                    TD_STRING+="\tLocation=(X="+str((ob.location.x-0.04)*TD_SCALE)+",Y="+str(-1*(ob.location.y+0.08)*TD_SCALE)+",Z="+str(ob.location.z*TD_SCALE)+")\n"        
             #360/65536 65536=2^16=2bytes
             isWall = False
             
